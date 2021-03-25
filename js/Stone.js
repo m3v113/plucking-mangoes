7be299b5a91e92b2f7dd5e382a73efb88efc8c8c
m3v113 <76668066+m3v113@users.noreply.github.com>
2021-03-25 10:32:25 -0700

    Update Stone.js

--- js/Stone.js
+++ js/Stone.js
@@ -1,5 +1,5 @@
 class Stone {
-    constructor(x,y,width,height) {
+    constructor(x,y,r) {
         var options = {
             isStatic: false,
             restitution: 0,
@@ -8,18 +8,26 @@ class Stone {
         }
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.r = r;
         
         this.image = loadImage("images/stone.png");
-        this.body = Bodies.rectangle(this.x, this.y, this.width, this.height, options);
+        this.body = Bodies.rectangle(this.x, this.y, this.r/2, options);
+	    
+	World.add(world, this.body); } 
     }
     display() {
 		var stoneSpot=this.body.position;	
 		push()
 		translate(stoneSpot.x, stoneSpot.y);
-        imageMode(CENTER);
-		image(this.image, 0, 0, this.width, this.height);
+	    	fill(255,0,255);
+	    	imageMode(CENTER);
+	    	ellipseMode(RADIUS);
+	    	image(this.image, 0,0,this.r*2, this.r*2);
 		pop()
     }
 }
+
+//class Stone { constructor(x,y,r) { var options={ isStatic:false, restitution:0, friction:1, density:1.2 } 
+//this.x=x; this.y=y; this.r=r this.image=loadImage("images/stone.png"); this.body=Bodies.circle(this.x, this.y, this.r/2, options) 
+//World.add(world, this.body); } display() { var stonepos=this.body.position; push() translate(stonepos.x, stonepos.y); // rectMode(CENTER) 
+// rotate(this.body.angle) fill(255,0,255) imageMode(CENTER); ellipseMode(RADIUS) image(this.image, 0,0,this.r*2, this.r*2) pop() } }
